98a7e6d1bf5904e5176afd3afa9b03b8079a2d19
Kyle Krattiger <mr.music25@gmail.com>
2016-12-01 16:42:28 -0800

    Tested function, it works! Included code used in a new fucntion, plus minor time-improvements.

--- m3uToUSB.sh
+++ m3uToUSB.sh
@@ -4,6 +4,11 @@
 # A bash implementation of my Powershell script, for when bash is available on Windows
 #
 # Changes:
+# v0.2.0
+# - Added testImport(), containing the code I used to successfully test importM3U()
+# - Changed importM3U() so that it tests for validity before converting, as it takes a LOT of cycles to complete
+# - Function also warns user that conversion process will take time
+#
 # v0.1.0
 # - Added multiple function definitions in prep for what's to come
 # - Untested version of importM3U() made with a new method I found
@@ -12,7 +17,7 @@
 # v0.0.1
 # - Initial commit
 #
-# v0.1.0, 01 Dec. 2016 14:43 PST
+# v0.2.0, 01 Dec. 2016 16:41 PST
 
 ### Variables
 
@@ -39,33 +44,68 @@ fi
 function convertSong() {
 	# Great idea from this website to convert files in parallel, quick conversion! Requires ffmpeg, and moreutils
 	# https://wiki.archlinux.org/index.php/Convert_Flac_to_Mp3
+	true
 }
 
 function displayHelp() {
-
+true
 }
 
 function deleteOldSongs() {
-
+true
 }
 
 function processArgs() {
-
+true
 }
 
 function importM3U() {
 	# Import the whole m3u file into an array, since it might need to be used multiple times
 	# http://stackoverflow.com/questions/21121562/shell-adding-string-to-an-array
+	
+	announce "Now preparing files for conversion..." "This process may take a minute depending on CPU power and playlist size"
 	while read -r line
 	do
 		[[ "$line" == \#* ]] && continue # Skip the line if it starts with a '#'
 		filePaths+=("$line")
-	done
+	done < "${m3uFile}"
 	
 	# Now that the lines are imported, make sure they are all Linux-friendly
 	for path in "${filePaths[@]}"
 	do
-		convertedPaths+=("$(win2UnixPath "$path" "$w2uMode")")
+		if [[ -f "$path" ]]; then
+			convertedPaths+=("$path") # This should save a lot of time with native Linux m3u files
+		else
+			convertedPaths+=("$(win2UnixPath "$path" "$w2uMode")")
+		fi
+	done
+}
+
+function testImport() {
+	#if [[ -z $1 || ! -f "$1" ]]; then
+	#	echo "Please run with a file!"
+	#	exit 1
+	#fi
+
+	#m3uFile="$1"
+	importM3U
+
+	echo " "
+	echo "Showing contents of filePaths..."
+	pause
+	for item in "${filePaths[@]}"
+	do
+		echo "$item"
+	done
+
+	pause
+
+	echo " "
+	echo "Showing contents of convertedPaths..."
+	pause
+	for items in "${convertedPaths[@]}"
+	do
+		echo "$items"
 	done
 }
 
