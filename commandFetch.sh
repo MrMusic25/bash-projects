7fd4ba78e8662b5e44ac38904dc789c2a8ab67e1
Kyle Krattiger <mr.music25@gmail.com>
2017-02-02 18:02:12 -0800

    Finished writing instalScript(), ready to start writing the part tat actually does stuff

--- commandFetch.sh
+++ commandFetch.sh
@@ -5,6 +5,12 @@
 # If it finds its name in the exclude list of the downloaded script, it will exit
 #
 # Changes:
+# v0.3.0
+# - Got installScript() up and running
+# - Coded daemon options
+# - Added more debug messages
+# - Made a small "workaround" to install cronjob as root... Just run as sudo!
+#
 # v0.2.0
 # - Fixed an error preventing script from running (SC1048)
 # - Added checks and warnings when the server is unset
@@ -28,7 +34,7 @@
 # - In daemon mode, no interactive stuff allowed (not that there should be much anways)
 # - Look for existing instances of this script before running, in case commands take a long time OR a loop is accidentally created, wasting CPU
 #
-# v0.2.0, 02 Feb. 2017 16:04 PST
+# v0.3.0, 02 Feb. 2017 18:02 PST
 
 ### Variables
 
@@ -36,6 +42,7 @@ hostname="$(cat /etc/hostname)" # Setup by default in every distro I have used
 server="$(cat /usr/share/server)" # Default place this script will store server address, which could be a static IP (local) or a hostname/domain (internet)
 serverPort=80 # This makes firewall handling easier. 8080 and 443 might be other good options, might be used later in this script
 defaultInterval=1 # Number of minutes between checks. Only used when setting up cron
+daemon=0
 
 ### Functions
 
@@ -70,13 +77,15 @@ Meant to be run as-is, but uses modifiers as necessary
 
 Options:
 -h | --help                  : Display this help message and exit
+-d | --daemon                : Stops interactive functions from running, make sure it is included in cronjobs!
 -s | --server <address>      : Specify the IP address/host/domain name to check with. Using this will NOT update the default address!
 -p | --port <port_num>       : Specify the port to use with wget
 -h | --hostname <name>       : Changes the hostname to check against the server with
--i | --install [root]        : Installs this script as a cron job, with option to do so as root
+-i | --install [user]        : Installs this script as a cron job, with option to do so as current user (default is to install as root)
 -v | --verbose               : Enable verbose mode. Note: MUST be the first argument!
 
 When script is first run, a cronjob will be automatically added for the user! Be careful when forcing it with -i !
+Default is to install as root; therefore, firt time run must be done with sudo or as root! (Same when running -i|--install)
 Using the -s option will not update the default server, located at /usr/share/server. Update this manually.
 Uses hostname found at /etc/hostname.
 
@@ -103,6 +112,7 @@ function processArgs() {
 				exit 1
 			fi
 			server="$2"
+			debug "INFO: Server temporarily set to: $server"
 			shift
 			;;
 			-p|--port)
@@ -114,7 +124,12 @@ function processArgs() {
 				exit 1
 			fi
 			port="$2"
+			debug "INFO: Port temporarily set to $port"
 			shift
+			;;
+			-d|--daemon)
+			debug "Daemon mode enabled"
+			daemon=1
 			;;
 			-h|--hostname)
 			if [[ -z $2 ]]; then
@@ -122,10 +137,12 @@ function processArgs() {
 				exit 1
 			fi
 			hostname="$2" # Puts a lot of trust in the user, same as with server
+			debug "INFO: Hostname temporarily set to $hostname"
+			shift
 			;;
 			-i|--install)
-			if [[ "$2" == "root" ]]; then
-				installScript "root"
+			if [[ "$2" == "user" ]]; then
+				installScript "user"
 				shift
 			else
 				installScript
@@ -141,15 +158,58 @@ function processArgs() {
 }
 
 function installScript() {
-	true
+	# Skip this if daemon mode on
+	if [[ $daemon -ne 0 ]]; then
+		return
+	fi
+	
+	announce "Beginning first-time setup!" "Root privileges will be required to make links/edit files" "Please enter password at prompts!"
+	if [[ ! -e /usr/bin/commandFetch ]]; then
+		sudo ln -s "$(pwd)"/"$0" /usr/bin/commandFetch
+	fi
+	
+	if [[ -e /usr/share/server ]]; then
+		getUserAnswer "n" "Default server is $(cat /usr/share/server), would you like to change it?"
+		case $? in
+			0)
+			true # Continue to rest of script, which will ask to set a new server
+			;;
+			1)
+			debug "Leaving default server as: $(cat /usr/share/server)"
+			return
+			;;
+			*)
+			debug "l2" "ERROR: Unkown return value $? for getUserAnswer! Please try again."
+			exit 1
+			;;
+		esac
+	fi
+	
+	# Setup cronjob, but first ask if user wants to change update interval
+	if [[ "$1" == "user" ]]; then
+		debug "l3" "INFO: Cronjob will be setup for current user instead of root!"
+	elif [[ "$EUID" -ne 0 ]]; then
+		# I hate to do it this way, but at the time I couldn't find a way to do this without rewriting everything
+		debug "l3" "ERROR: Please run this script as sudo/root to install with privilege!"
+		exit 1
+	fi
+		
+	# Again, this is why I love functions
+	getUserAnswer "n" "Would you like to change the default time between checks from $defaultInterval minute(s)?" defaultInterval "How many minutes between checks?"
+	addCronJob "$defaultInterval" min "/usr/bin/commandFetch -d"
+	
+	# At this point, either the default server is unset or the user wishes to change it
+	read -p "Please type the IP address, hostname, or domain name of the default server: " server
+	debug "Changing default server to: $server"
+	echo "$server" | sudo tee /usr/share/server > /dev/null
 }
 
 ### Main Script
 
 # Link the script to /usr/bin if not already there
-if [[ ! -e /usr/bin/commandFetch.sh ]]; then
+if [[ ! -e /usr/bin/commandFetch ]]; then
 	debug "l3" "Script is not linked to /usr/bin, please give root permissions to complete!"
-	sudo ln -s "$(pwd)"/"$0" /usr/bin/commandFetch.sh
+	sudo ln -s "$(pwd)"/"$0" /usr/bin/commandFetch
 fi
 
 processArgs "$@" # Check to see if server given here before exiting
